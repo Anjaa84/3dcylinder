92a3f6e5ebccd40f84224ee960da4bfd85e91df9
ANJANAN MANIKAVASAGAR <anjananmy@gmail.com>
2023-03-10 23:42:44 +0530

    added style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Cylinder3d from "./component/3dCylinder";
- 
+
 function App() {
   return (
     <>
-      <section className='App-header'>
+      <section className="App-header">
+        {/* Canvas 1 */}
         <Canvas>
-          {/* <pointLight position={[10, 10, 10]} /> */}
-          {/* <ambientLight /> */}
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight />
+          <Cylinder3d position={[-1.2, 0, 0]} />
+          <Cylinder3d position={[1.2, 0, 0]} />
+        </Canvas>
+
+        {/* Canvas 2 */}
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight intensity={0.5} />
+          <Cylinder3d position={[-1.2, 0, 0]} wireframe={true} />
+          <Cylinder3d position={[1.2, 0, 0]} wireframe={true} />
+        </Canvas>
+
+        {/* Canvas 3 */}
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight color={"red"} />
           <Cylinder3d position={[-1.2, 0, 0]} />
           <Cylinder3d position={[1.2, 0, 0]} />
         </Canvas>
@@ -16,5 +33,5 @@ function App() {
     </>
   );
 }
- 
+
 export default App;
